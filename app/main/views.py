10a065d3ea7b13e991d5ffa74229d2eb9d98b038
Sieva-cmd <musyokasieva@gmail.com>
2022-03-12 02:01:57 +0300

    add update profile

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,10 @@
 
-from flask import render_template
+from flask import render_template,redirect,request,url_for,abort
 from . import main
 from flask_login import login_required
+from ..models import User
+from .forms import UpdateProfile
+from .. import db
 
 #views
 @main.route('/')
@@ -11,3 +14,33 @@ def index():
     title = 'My blog'
 
     return render_template('index.html',title=title)
+
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user =User.query.filter_by(username =uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template('profile/profile.html',user=user) 
+
+
+@main.route('/user/<uname>/update',methods=["GET","POST"])   
+@login_required
+def update_profile(uname):
+    user =User.query.filter_by(username =uname).first()
+    if user is None:
+        abort(404)
+
+    form =UpdateProfile()
+
+    if form.validate_on_submit():
+        user.bio =form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('main.profile',uname=user.username))
+
+    return render_template('profile/update.html',form =form)                
