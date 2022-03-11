ab621797ab1f916384bf6cb97145ff20d3b61f8f
Sieva-cmd <musyokasieva@gmail.com>
2022-03-11 20:23:31 +0300

    hashing pssword

--- app/models.py
+++ app/models.py
@@ -9,6 +9,18 @@ class User(db.Model):
     pass_secure =db.Column(db.String(255))
 
 
+    @property
+    def password(self):
+        raise AttributeError('You can not read the password atribute')
+
+    @password.setter
+    def password(self,password):
+        self.pass_secure =generate_password_hash(password)
+
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)        
+
+
 
 
     def __repr__(self):
